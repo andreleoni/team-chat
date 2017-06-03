25c786d9902c6a0a9ee4114182313502611434b9
André Luiz Leoni <andreluizleoni@gmail.com>
2017-06-03 14:42:48 -0300

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # get 'team_users/create'
+  # get 'team_users/destroy'
+  # get 'talks/show'
+  # get 'channels/create'
+  # get 'channels/destroy'
+  # get 'channels/show'
+  # get 'teams/index'
+  # get 'teams/show'
+  # get 'teams/create'
+  # get 'teams/destroy'
+
+  resources :channels, only: [:show, :create, :destroy]
+  resources :talks, only: [:show]
+  resources :teams, only: [:create, :destroy]
+  resources :team_users, only: [:create, :destroy]
+
+  get '/:slug', to: 'teams#show'
+
+  root to: 'teams#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
 end
